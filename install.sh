48eafe40ce024b721ad88c6b48e6e59fcb6d7a3a
Sergey Litvinov <slitvinov@gmail.com>
2020-10-30 12:31:47 +0100

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+
+(cd netcdf &&
+./configure &&
+make 'FC = mpifort' 'FCFLAGS = -O2 -g -fallow-argument-mismatch' &&
+make install 'prefix = $(HOME)/.local')
+
+(cd metis
+ make install)
+
+(cd vizing
+ make install)
+
+(cd ppm
+ make install)
+
+(cd medusa
+ make)
