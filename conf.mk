0c6559da4c1d9f7e5be0066f3d90cdfb0bd01dbd
Sergey Litvinov <slitvinov@gmail.com>
2020-10-31 10:21:58 +0100

    rm -free-form

--- conf.mk
+++ conf.mk
@@ -6,4 +6,4 @@ PREFIX = $(HOME)/.local
 FC = mpifort
 FCFLAGS = -O2 -g
 # gfortran
-FXFLAGS = -cpp -ffree-line-length-none -fallow-argument-mismatch -ffree-form
+FXFLAGS = -cpp -ffree-line-length-none -fallow-argument-mismatch
