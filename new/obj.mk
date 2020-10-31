5df8975c8bb3ca0c7c22cdc337b6d81e4ed4cff9
Sergey Litvinov <slitvinov@gmail.com>
2020-10-31 12:18:35 +0100

    initial import of new/

---
+++ new/obj.mk
@@ -0,0 +1,13 @@
+O = \
+module_netcdf_nc_data.o\
+module_netcdf_nc_interfaces.o\
+module_netcdf_nf_data.o\
+module_netcdf_nf_interfaces.o\
+module_netcdf_f03.o\
+module_netcdf_fortv2_c_interfaces.o\
+module_netcdf4_nc_interfaces.o\
+module_netcdf4_nf_interfaces.o\
+module_netcdf4_f03.o\
+typeSizes.o\
+netcdf4.o\
+
