9ad0e4b9a101b4d9959a34d694f7bfd56f3e40c6
Sergey Litvinov <slitvinov@gmail.com>
2020-10-31 10:45:17 +0100

    do not need -ffree-line-length-none

--- conf.mk
+++ conf.mk
@@ -1,9 +1,9 @@
+PREFIX = $(HOME)/.local
 CC = c99
 CFLAGS = -O2 -g
 CXX = g++
 CXXFLAGS = -O2 -g
-PREFIX = $(HOME)/.local
 FC = mpifort
 FCFLAGS = -O2 -g
 # gfortran
-FXFLAGS = -cpp -ffree-line-length-none -fallow-argument-mismatch
+FXFLAGS = -cpp -fallow-argument-mismatch
